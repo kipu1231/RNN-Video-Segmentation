f91158623ee08aa6ea14c2e1f3a8318e30d466cb
Chiara Pullem <chiara.pullem@gmx.de>
2020-04-03 13:59:51 +0200

    update of bash for preprocess

--- get_preprocess_data.sh
+++ get_preprocess_data.sh
@@ -1,6 +1,6 @@
 # Download preprocessing from Dropbox
-wget -P /preprocess/RNN https://www.dropbox.com/s/ln6uwnbo7r992qk/features_res.pkl
-wget -P /preprocess/RNN https://www.dropbox.com/s/smk2t1avgobld5l/features_val_res.pkl
+wget -P preprocess/RNN https://www.dropbox.com/s/ln6uwnbo7r992qk/features_res.pkl
+wget -P preprocess/RNN https://www.dropbox.com/s/smk2t1avgobld5l/features_val_res.pkl
 
-wget -P /preprocess/s2s hhttps://www.dropbox.com/s/91tzrxqn3zlrtgh/features_train_p3.pkl
-wget -P /preprocess/s2s https://www.dropbox.com/s/7ty42urxsb0x53d/features_val_p3.pkl
+wget -P preprocess/S2S https://www.dropbox.com/s/91tzrxqn3zlrtgh/features_train_p3.pkl
+wget -P preprocess/S2S https://www.dropbox.com/s/7ty42urxsb0x53d/features_val_p3.pkl
